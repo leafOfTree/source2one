9fc5d75d6eea8a25a99bce85d376bbfe59d70fb9
yemai <node_github@163.com>
2018-05-14 15:09:02 +0800

    feat: add parseArgs and slugify targetFile.

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ var generate = require('./lib');
 main();
 
 function main() {
-    var sourceDir = process.argv[2];
+    var argObj = parseArgs();
+    console.log(argObj)
+    var sourceDir = argObj.sourceDir;
+    var ignoreFiles = argObj.i;
+
     if (!sourceDir) {
         console.log('Source dir is required');
     } else {
-        var toc = {
+        var targetFile = `${sourceDir.replace(/(\\|\/)/g, '_')}.source.md`;
+        var tocConfig = {
             sourceDir,
-            targetFile: `${sourceDir}.source.md`,
+            ignoreFiles, 
+            targetFile,
             title: '## TOC\n',
             contentTitle: '\n## Content\n',
             node: {
@@ -21,6 +27,29 @@ function main() {
             }
         };
 
-        generate(toc);
+        generate(tocConfig);
     }
+}
+
+function parseArgs() {
+    var args = process.argv.slice(2);
+    var argObj = {};
+    for (var i = 0 ; i < args.length; i++) {
+        ['i'].forEach(function (opt) {
+            if (args[i] === ('-' + opt) && args[i + 1]) {
+                argObj[opt] = args[i + 1];
+                args[i] = args[i + 1] = null;
+                i++;
+            } else if (args[i].match(/-.*/)) {
+                console.log('Error! Unsupported option: ' + args[i])
+                process.exit(1)
+            }
+        })
+    }
+    args.forEach(function (arg) {
+        if (arg) {
+            argObj['sourceDir'] = arg;
+        }
+    })
+    return argObj;
 }
